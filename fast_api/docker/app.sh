8ef2e03442e02bad4991b6fbd25c72138c731578
Konstantin Pershin <kostya.pershin.18@mail.ru>
2024-03-22 13:06:23 +0500

    add submodule fast_api

--- fast_api/docker/app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-alembic upgrade head
-
-uvicorn main:app --host 0.0.0.0 --port 8000
--- fast_api/src/llm_service/utils.py
+++
@@ -1,8 +0,0 @@
-import httpx
-
-
-async def send_data_to_llm(endpoint: str, data: dict):
-    async with httpx.AsyncClient(timeout=30) as client:
-        url = f"http://gigachat_api:8080/{endpoint}"
-        response = await client.post(url, json=data)
-        return response.json()
